9d2f0bb45f1d740aa3b9c192e81a1137b4fb9302
Joel Bartlett <joel@murbar.com>
2019-03-28 13:32:47 -0400

    Re-org knex config

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,15 @@
 module.exports = {
-  client: 'sqlite3',
-  useNullAsDefault: true,
-  connection: {
-    filename: './data/lambda.sqlite3'
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/lambda.sqlite3'
+    },
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/migrations'
+    }
   }
 };
